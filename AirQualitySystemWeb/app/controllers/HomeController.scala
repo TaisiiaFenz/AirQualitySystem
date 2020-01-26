ba8edc595c3d33b20014a9c773488b9eedf5f218
jlareck <medik2507@gmail.com>
2020-01-27 00:56:16 +0200

    HomeController handles connections

--- AirQualitySystemWeb/app/controllers/HomeController.scala
+++ AirQualitySystemWeb/app/controllers/HomeController.scala
@@ -15,34 +15,30 @@ import play.api.libs.concurrent.CustomExecutionContext
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents, db: Database, executionContext: DatabaseExecutionContext)(implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents)(implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
 
 
   def index = Action {
-    Ok(views.html.index("ffdas"))
+    Ok(views.html.index(""))
   }
 
 
 
   def socket = WebSocket.accept[String, String] { request =>
     ActorFlow.actorRef { out =>
-      MyWebSocketActor.props(out, executionContext, db)
+      MyWebSocketActor.props(out)
 
     }
   }
 
-  def arduinodata = WebSocket.accept[String, String] { request =>
-    ActorFlow.actorRef { out =>
-      MyWebSocketActor.props(out,executionContext,db)
 
-    }
-  }
 
 }
 import akka.actor._
 object Senders{
    var senders: Set[ActorRef] = Set.empty
 }
+// TODO: Implement connection handler
 object ConnectionHandler {
   case object Join
   case class ChatMessage(message: String)
@@ -50,47 +46,18 @@ object ConnectionHandler {
 
 
 object MyWebSocketActor {
-  def props(out: ActorRef, executionContext: DatabaseExecutionContext, db: Database) = Props(new MyWebSocketActor(out,executionContext,db))
+  def props(out: ActorRef) = Props(new MyWebSocketActor(out))
 
 }
 
-class MyWebSocketActor (out: ActorRef, executionContext: DatabaseExecutionContext, db: Database) extends Actor {
+class MyWebSocketActor (out: ActorRef) extends Actor {
 
   def receive = {
     case msg:  String => {
-        Senders.senders+=sender()
-     //   println(msg)
-        if(isAllDigits(msg)){
-          updateSomething(msg.toInt)
-        }
+      Senders.senders+=out
 
     }
 
   }
-  def isAllDigits(x: String) = x forall Character.isDigit
-  def updateSomething(msg: Int): Unit = {
-
-    Future {
-      // get jdbc connection
-      val connection = db.getConnection()
-
-      val sql: String = "INSERT INTO sensorTable (value)" + "VALUES(?)"
-      val preparedStatement = connection.prepareStatement(sql)
-      preparedStatement.setInt(1,msg)
-      preparedStatement.executeUpdate()
-      println(msg)
-      //val statement = connection.cre
-      // do whatever you need with the db connection
-
-      // remember to close the connection
-      connection.close()
-    }(executionContext)
-  }
-  //out ! ("fda")
 }
-//case object SomeMessage
-//class TestWebsocketActor extends Actor {
-//  def receive  ={
-//    case SomeMessage => MyWebSocketActor ! "a"
-//  }
-//}
+
